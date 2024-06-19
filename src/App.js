f7841e970f92f88a0cf5a7bcdd26fbc221e120ea
Benjamin Mah <benjaminmah2004@gmail.com>
2024-06-19 00:54:02 -0400

    Working about page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import About from './components/about';
-import Layout from './components/layout'; // Adjust the path as necessary
+import Layout from './components/layout';
 import { MusicPlayerProvider } from './MusicPlayerContext';
 
 const App = () => {
--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,54 @@ import '../styles/about.css';
 
 const About = () => {
     return (
-        <div className="about">
-            <h1>About Me</h1>
-            <p>Hello! I'm Benjamin Mah, a passionate software developer with a focus on machine learning and web development. I love creating innovative solutions and building projects that make a difference. Welcome to my personal website where you can learn more about my work and interests.</p>
+        <div className="about-me">
+            <div className="sticky-bar"></div> 
+            <div className="about-container">
+                <div className="about-text">
+                    <h1>about me</h1>
+                    <p>hello! my name is Benjamin Mah and I'm a machine intelligence engineering student at the University of Toronto. I'm passionate about harnessing the potential of machine learning to create impactful solutions ...</p>
+                    <p>... but when I'm not on my computer, you can find me collecting records, producing music, or playing basketball.</p>
+                </div>
+                <img src="/icons/me1.png" alt="me" className="about-image" />
+            </div>
+            <div className="experiences-container">
+                <img src="/icons/brain1.png" alt="me" className="experiences-image" />
+                <div className="experiences">
+                    <h1>experiences</h1>
+                    <p>currently, I'm working at Mozilla (Firefox) as a machine learning intern on the ci and quality tools team!</p>
+                    <p>previously:</p>
+                    <ul>
+                        <li>
+                            <p>data specialist at Cohere</p>
+                        </li>
+                        <li>
+                            <p>machine learning intern at RBC</p>
+                        </li>
+                        <li>
+                            <p>co-founder of Flipdoor, backed by UofT Hatchery</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="projects-container">
+                <div className="projects">
+                    <h1>projects</h1>
+                    <p>all my projects can be found on my github page, linked under the contact section of my website ;)</p>
+                    <p>here are a few of my favourites:</p>
+                    <ul>
+                        <li>
+                            <p><strong>accent style transfer (github repo, paper):</strong> researched a new method of style transfer using CNNs (results were surprising!)</p>
+                        </li>
+                        <li>
+                            <p><strong>kidney image segmentation (github repo, paper):</strong> used a feature pyramid network to segment vasculature in kidney ct scans</p>
+                        </li>
+                        <li>
+                            <p><strong>dubu chatbot (github repo, demo):</strong> created a customizable chatbot based on user pdf uploads and tone selection</p>
+                        </li>
+                    </ul>
+                </div>
+                <img src="/icons/nerd1.png" alt="me" className="projects-image" />
+            </div>
         </div>
     )
 }
