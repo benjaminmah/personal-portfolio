5cf0c3bd878a51e618b3ac2ca628e4af6783c050
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-29 11:43:19 -0400

    Changed song

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ const About = () => {
                 <div className="about-text">
                     <h1>about me</h1>
                     <p>hello! my name is Benjamin Mah and I'm a machine intelligence engineering student at the University of Toronto. I'm passionate about harnessing the potential of machine learning to create impactful solutions, but when I'm not at my computer ...</p>
-                    <p>... you can find me collecting records, learning how to produce music, playing basketball (+ watching the raptors), or sleeping</p>
+                    <p>... you can find me collecting records, learning how to produce music, playing basketball, or sleeping</p>
                 </div>
                 <img src="/icons/me1.png" alt="me" className="about-image" />
             </div>
