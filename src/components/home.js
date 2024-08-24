a70d094467b72f0b1d59cf74e1c6e161a610d884
Benjamin Mah <benjaminmah2004@gmail.com>
2024-08-24 01:04:02 -0400

    Changed song

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <div className="home">
             <h1>hi! i'm benjamin</h1>
             <p className="blurb">
-                Jupiter - Matt Maltese
+                Beep-Boop, Beep-Boop - Meaningful Stone
                 <span className={`music-player ${isPlaying ? 'playing' : ''}`} onClick={togglePlayPause}>
                     <img src={isPlaying ? `${baseUrl}/icons/pause1.png` : `${baseUrl}/icons/play1.png`} alt="Music Player" className="music-icon" />
                 </span>
